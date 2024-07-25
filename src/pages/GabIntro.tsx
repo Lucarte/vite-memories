6ae8e391efc68c9e4b04e36d3c9b2712fada4a62
LucArte <96082778+Lucarte@users.noreply.github.com>
2024-07-25 20:36:26 +0200

    Gabis Intro styled

--- src/pages/GabIntro.tsx
+++ src/pages/GabIntro.tsx
@@ -3,105 +3,124 @@ import { Link } from "react-router-dom";
 
 const GabIntro: React.FC = () => {
 	return (
-		<div className='relative flex flex-col items-center h-screen gap-6 overflow-hidden text-center'>
-			<h1 className='pb-6 text-xl font-bold underline uppercase'>
-				.g.a.b.r.i.e.l.l.a.
+		<div className='flex flex-col items-center h-[84vh] overflow-hidden text-center'>
+			<h1 className='pb-6 text-xl font-bold tracking-wider underline uppercase'>
+				.g.a.b.i.
 			</h1>
-			<div className='absolute w-56 h-56 mt-16 border border-gray-100'>
-				<div className='text-xl text-center text-black uppercase dark:text-white'>
-					<p className='top-0 left-0 h-6 border-b w-fit border:white'>
-						Music Theater
+			<div className='absolute mt-16 w-52 h-52'>
+				<div className='top-0 left-0 text-xl tracking-widest text-center text-black uppercase dark:text-white'>
+					<p className='h-6 pt-[.1rem] pl-[.1rem] underline w-fit'>
+						MUSIC-THEATER
 					</p>
-					<p className='absolute right-0 h-6 mt-[4.4rem] -mr-4 rotate-90 border-b w-fit border:white'>
-						Music Theater
+					<p className='absolute right-0 h-6 mt-[3.9rem] -mr-[5.0rem] rotate-90 underline text-black dark:text-white text-opacity-95 dark:text-opacity-95'>
+						MUSIC-THEATER
 					</p>
-					<p className='absolute h-6 ml-[1.2rem] rotate-180 border-b mt-[10rem] w-fit border:white'>
-						Music Theater
+					<p className='absolute h-6 ml-[1.5rem] rotate-180  mt-[10.7rem] underline text-black dark:text-white text-opacity-90 dark:text-opacity-90'>
+						MUSIC-THEATER
 					</p>
-					<p className='absolute w-fit h-6 rotate-[270deg] mt-[5.6rem] -ml-[4.4rem] border-b border:white'>
-						Music Theater
-					</p>
-				</div>
-				{/* <div className='relative w-48 text-xl text-center text-white uppercase bg-orange-500'>
-					<p className='absolute h-6 border-b left-[.6rem] top-4 w-fit border:white'>
-						B r u n n i
-					</p>
-					<p className='absolute h-6 mt-[3.6rem] ml-[4.6rem] rotate-90 border-b w-fit border:white'>
-						B r u n n i
-					</p>
-					<p className='absolute h-6 ml-[1.9rem] rotate-180 border-b mt-[7.7rem] w-fit border:white'>
-						B r u n n i
-					</p>
-					<p className='absolute w-fit h-6 rotate-[270deg] mt-[5rem] -ml-[2.1rem] border-b border:white'>
-						B r u n n i
+					<p className='absolute rotate-[270deg] mt-[5.2rem] -ml-[5.2rem] underline text-black dark:text-white text-opacity-85  dark:text-opacity-85'>
+						MUSIC-THEATER
 					</p>
 				</div>
-				<div className='relative w-48 text-xl text-center text-white uppercase bg-orange-500'>
-					<p className='absolute h-6 border-b left-[.6rem] top-4 w-fit border:white'>
-						Music
+				<div className='relative left-[2.1rem] top-[.45rem] tracking-wider text-xl text-center text-black uppercase dark:text-white'>
+					<p className='absolute h-6 -mt-1 -ml-2 text-black underline dark:text-white text-opacity-80 dark:text-opacity-85'>
+						B r u n n i s
 					</p>
-					<p className='absolute h-6 mt-[3.6rem] ml-[4.6rem] rotate-90 border-b w-fit border:white'>
-						Music
+					<p className='absolute mt-[3.4rem] h-6 ml-[4.8rem] rotate-90 underline text-black dark:text-white text-opacity-75 dark:text-opacity-80'>
+						B r u n n i s
 					</p>
-					<p className='absolute h-6 ml-[1.9rem] rotate-180 border-b mt-[7.7rem] w-fit border:white'>
-						Music
+					<p className='h-6 absolute ml-[1.2rem] rotate-180 mt-[8.6rem] underline text-black dark:text-white text-opacity-70 dark:text-opacity-75'>
+						B r u n n i s
 					</p>
-					<p className='absolute w-fit h-6 rotate-[270deg] mt-[5rem] -ml-[2.1rem] border-b border:white'>
-						Music
+					<p className='absolute h-6 rotate-[270deg] mt-[5rem] -ml-[4rem] underline text-black dark:text-white text-opacity-65 dark:text-opacity-70'>
+						B r u n n i s
 					</p>
-				</div> */}
+				</div>
+				<div className='relative text-xl text-center text-black uppercase dark:text-white'>
+					<p className='absolute h-6 left-[3.4rem] top-[1.7rem] underline text-black dark:text-white text-opacity-60 dark:text-opacity-65'>
+						M u s i c
+					</p>
+					<p className='absolute h-6 mt-[3.9rem] ml-[6.9rem] rotate-90 underline text-black dark:text-white text-opacity-55 dark:text-opacity-60'>
+						M u s i c
+					</p>
+					<p className='absolute h-6 ml-[4.7rem] rotate-180 mt-[7.5rem] underline text-black dark:text-white text-opacity-50 dark:text-opacity-55'>
+						M u s i c
+					</p>
+					<p className='absolute h-6 rotate-[270deg] mt-[5.3rem] ml-[1.2rem] underline text-black dark:text-white text-opacity-45 dark:text-opacity-50'>
+						M u s i c
+					</p>
+				</div>
+				<div className='relative text-xl tracking-tight text-center uppercase'>
+					<p className='absolute h-6 left-[4.9rem] top-[3.2rem] underline text-black dark:text-white text-opacity-40 dark:text-opacity-45'>
+						Art
+					</p>
+					<p className='absolute h-6 mt-[4rem] ml-[6.9rem] rotate-90 underline text-black dark:text-white text-opacity-30 dark:text-opacity-40'>
+						Art
+					</p>
+					<p className='absolute h-6 ml-[6.1rem] rotate-180 mt-[6.1rem] underline text-black dark:text-white text-opacity-20 dark:text-opacity-30'>
+						Art
+					</p>
+					<p className='absolute h-6 rotate-[270deg] mt-[5.3rem] ml-[4rem] underline text-black dark:text-white text-opacity-10 dark:text-opacity-20'>
+						Art
+					</p>
+				</div>
 			</div>
 			<div className='mt-72'>
 				<Link
 					to='/memory/create'
-					className='relative inline-block h-[1.8rem] px-3 pt-3 text-white bg-black rounded-tl-lg rounded-tr-lg dark:bg-white dark:text-black'>
+					className='relative inline-block h-[1.8rem] px-3 pt-3 text-white bg-black rounded-tl-lg rounded-tr-lg dark:bg-white dark:text-black mb-6'>
 					<span className='text-white uppercase dark:text-black animate-bounceText'>
 						Add Memory
 					</span>
 				</Link>
-				<div className='relative overflow-hidden w-80 h-96'>
+				<div className='relative overflow-hidden w-80 h-[28rem]'>
 					<div className='absolute inset-0 flex flex-col gap-4'>
-						<div className='flex flex-col items-center gap-4 font-sans uppercase rotate-180'>
-							<h2 className='text-3xl font-bold underline'>VIOLA</h2>
-							<p className='text-xs'>
+						<div className='flex flex-col items-center gap-1 font-sans uppercase rotate-180'>
+							<h2 className='text-3xl font-bold underline'>DANCE</h2>
+							<p className='font-normal text-[.6rem] md:text-xs'>
 								"The information you consume each day <br />
 								is the soil from which your <br />
 								future thoughts are grown" <br />
 								<span className='font-medium'>James Clear</span>
 							</p>
 						</div>
-						<div className='text-[10px]'>
-							<p>PABLO</p>
+						<div className='text-[10px] -mt-5'>
+							<p>GABRIELLA</p>
 							<div className='-space-y-1 rotate-180'>
-								<p>P</p>
+								<p>G</p>
 								<p>A</p>
 								<p>B</p>
+								<p>R</p>
+								<p>I</p>
+								<p>E</p>
 								<p>L</p>
-								<p>O</p>
 								<p>L</p>
+								<p>A</p>
+								<p>L</p>
+								<p>L</p>
+								<p>E</p>
+								<p>I</p>
+								<p>R</p>
 								<p>B</p>
 								<p>A</p>
-								<p>P</p>
+								<p>G</p>
+								<p>G</p>
 								<p>A</p>
 								<p>B</p>
+								<p>R</p>
+								<p>I</p>
+								<p>E</p>
 								<p>L</p>
-								<p>O</p>
 								<p>L</p>
+								<p>A</p>
+								<p>L</p>
+								<p>L</p>
+								<p>E</p>
+								<p>I</p>
+								<p>R</p>
 								<p>B</p>
 								<p>A</p>
-								<p>P</p>
-								<p>A</p>
-								<p>B</p>
-								<p>L</p>
-								<p>O</p>
-								<p>L</p>
-								<p>B</p>
-								<p>A</p>
-								<p>P</p>
-								<p>A</p>
-								<p>B</p>
-								<p>L</p>
-								<p>O</p>
+								<p>G</p>
 							</div>
 						</div>
 					</div>
--- src/pages/PabloIntro.tsx
+++ src/pages/PabloIntro.tsx
@@ -3,59 +3,61 @@ import { Link } from "react-router-dom";
 const PabloIntro = () => {
 	return (
 		<div className='flex flex-col items-center h-[84vh] overflow-hidden text-center'>
-			<h1 className='pb-6 text-xl font-bold underline uppercase'>
+			<h1 className='pb-6 text-xl font-bold tracking-wider underline uppercase'>
 				.p.a.b.l.o.
 			</h1>
 			<div className='absolute mt-16 w-52 h-52'>
-				<div className='top-0 left-0 ml-2 text-xl tracking-widest text-center text-black uppercase dark:text-white'>
+				<div className='top-0 left-0 text-xl tracking-[.2rem] text-center text-black uppercase dark:text-white'>
 					<p className='h-6 underline w-fit'>PROGRAMMING</p>
-					<p className='absolute right-0 h-6 mt-[3.4rem] -mr-[4.6rem] rotate-90 underline'>
+					<p className='absolute right-0 h-6 mt-[3.9rem] -mr-[5.1rem] rotate-90 underline text-black dark:text-white text-opacity-95 dark:text-opacity-95'>
 						PROGRAMMING
 					</p>
-					<p className='absolute h-6 ml-[1.4rem] rotate-180  mt-[9.9rem] underline'>
+					<p className='absolute h-6 ml-[1.5rem] rotate-180  mt-[10.7rem] underline text-black dark:text-white text-opacity-90 dark:text-opacity-90'>
 						PROGRAMMING
 					</p>
-					<p className='absolute rotate-[270deg] mt-[4.8rem] -ml-[4.8rem] underline'>
+					<p className='absolute rotate-[270deg] mt-[5.2rem] -ml-[5.2rem] underline text-black dark:text-white text-opacity-85  dark:text-opacity-85'>
 						PROGRAMMING
 					</p>
 				</div>
 				<div className='relative left-[2.1rem] top-[.45rem] tracking-wider text-xl text-center text-black uppercase dark:text-white'>
-					<p className='absolute h-6 -mt-2 -ml-1 underline'>B r u n n i s</p>
-					<p className='absolute mt-[3.1rem] h-6 ml-[4.6rem] rotate-90 underline'>
+					<p className='absolute h-6 -mt-1 -ml-2 text-black underline dark:text-white text-opacity-80 dark:text-opacity-85'>
 						B r u n n i s
 					</p>
-					<p className='h-6 absolute ml-[1.1rem] rotate-180 mt-[8.1rem] underline'>
+					<p className='absolute mt-[3.4rem] h-6 ml-[4.8rem] rotate-90 underline text-black dark:text-white text-opacity-75 dark:text-opacity-80'>
 						B r u n n i s
 					</p>
-					<p className='absolute h-6 rotate-[270deg] mt-[4.5rem] -ml-[3.8rem] underline'>
+					<p className='h-6 absolute ml-[1.2rem] rotate-180 mt-[8.6rem] underline text-black dark:text-white text-opacity-70 dark:text-opacity-75'>
+						B r u n n i s
+					</p>
+					<p className='absolute h-6 rotate-[270deg] mt-[5rem] -ml-[4rem] underline text-black dark:text-white text-opacity-65 dark:text-opacity-70'>
 						B r u n n i s
 					</p>
 				</div>
-				<div className='relative text-xl tracking-widest text-center text-black uppercase dark:text-white'>
-					<p className='absolute h-6 left-[3.4rem] top-[1.5rem] underline'>
-						Music
+				<div className='relative text-xl text-center text-black uppercase dark:text-white'>
+					<p className='absolute h-6 left-[3.4rem] top-[1.7rem] underline text-black dark:text-white text-opacity-60 dark:text-opacity-65'>
+						M u s i c
 					</p>
-					<p className='absolute h-6 mt-[3.5rem] ml-[7rem] rotate-90 underline'>
-						Music
+					<p className='absolute h-6 mt-[3.9rem] ml-[6.9rem] rotate-90 underline text-black dark:text-white text-opacity-55 dark:text-opacity-60'>
+						M u s i c
 					</p>
-					<p className='absolute h-6 ml-[5rem] rotate-180 mt-[7.1rem] underline'>
-						Music
+					<p className='absolute h-6 ml-[4.7rem] rotate-180 mt-[7.5rem] underline text-black dark:text-white text-opacity-50 dark:text-opacity-55'>
+						M u s i c
 					</p>
-					<p className='absolute h-6 rotate-[270deg] mt-[5.1rem] ml-[1.4rem] underline'>
-						Music
+					<p className='absolute h-6 rotate-[270deg] mt-[5.3rem] ml-[1.2rem] underline text-black dark:text-white text-opacity-45 dark:text-opacity-50'>
+						M u s i c
 					</p>
 				</div>
-				<div className='relative text-xl tracking-tight text-center text-black uppercase dark:text-white'>
-					<p className='absolute h-6 left-[4.7rem] top-[2.8rem] underline'>
+				<div className='relative text-xl tracking-tight text-center uppercase'>
+					<p className='absolute h-6 left-[4.9rem] top-[3.2rem] underline text-black dark:text-white text-opacity-40 dark:text-opacity-45'>
 						Art
 					</p>
-					<p className='absolute h-6 mt-[3.6rem] ml-[6.7rem] rotate-90 underline'>
+					<p className='absolute h-6 mt-[4rem] ml-[6.9rem] rotate-90 underline text-black dark:text-white text-opacity-30 dark:text-opacity-40'>
 						Art
 					</p>
-					<p className='absolute h-6 ml-[5.8rem] rotate-180 mt-[5.7rem] underline'>
+					<p className='absolute h-6 ml-[6.1rem] rotate-180 mt-[6.1rem] underline text-black dark:text-white text-opacity-20 dark:text-opacity-30'>
 						Art
 					</p>
-					<p className='absolute h-6 rotate-[270deg] mt-[4.9rem] ml-[3.8rem] underline'>
+					<p className='absolute h-6 rotate-[270deg] mt-[5.3rem] ml-[4rem] underline text-black dark:text-white text-opacity-10 dark:text-opacity-20'>
 						Art
 					</p>
 				</div>
@@ -63,14 +65,14 @@ const PabloIntro = () => {
 			<div className='mt-72'>
 				<Link
 					to='/memory/create'
-					className=' mb-6 inline-block h-[1.8rem] px-3 pt-3 text-white bg-black rounded-tl-lg rounded-tr-lg dark:bg-white dark:text-black'>
+					className='mb-6 inline-block h-[1.8rem] px-3 pt-3 text-white bg-black rounded-tl-lg rounded-tr-lg dark:bg-white dark:text-black'>
 					<span className='text-white uppercase dark:text-black animate-bounceText'>
 						Add Memory
 					</span>
 				</Link>
 				<div className='relative overflow-hidden w-80 h-[28rem]'>
-					<div className='absolute inset-0 flex flex-col gap-4'>
-						<div className='flex flex-col items-center gap-4 font-sans uppercase rotate-180'>
+					<div className='absolute inset-0 flex flex-col'>
+						<div className='flex flex-col items-center gap-1 font-sans uppercase rotate-180'>
 							<h2 className='text-3xl font-bold underline'>VIOLA</h2>
 							<p className='font-normal text-[.6rem] md:text-xs'>
 								"For each headache you face, ask youself: <br />
@@ -80,7 +82,7 @@ const PabloIntro = () => {
 								<br /> <span className='font-medium'>James Clear</span>
 							</p>
 						</div>
-						<div className='text-[10px]'>
+						<div className='text-[10px] -mt-1'>
 							<p>PABLO</p>
 							<div className='-space-y-1 rotate-180'>
 								<p>P</p>
