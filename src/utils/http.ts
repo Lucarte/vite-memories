38ddd447fd04d4648fc3069983b547de39f3e660
LucArte <96082778+Lucarte@users.noreply.github.com>
2024-10-16 06:57:59 +0200

    update http

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-	baseURL: import.meta.env.VITE_API_URL || "https://104.248.123.220", // Fallback for development or troubleshooting
+	baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000", // Fallback for development or troubleshooting
 	withCredentials: true,
 	withXSRFToken: true,
 });
