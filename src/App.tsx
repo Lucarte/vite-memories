59452605ae1541a58adbd9964bd9d68b9b7e55bd
LucArte <96082778+Lucarte@users.noreply.github.com>
2024-10-29 14:05:58 +0100

    more debug tries

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import "./App.css";
 import Header from "./partials/Header";
 
 const App = () => {
+	console.log(
+		import.meta.env.VITE_MEILISEARCH_HOST,
+		import.meta.env.VITE_MEILISEARCH_API_KEY
+	);
 	return (
 		<div className=''>
 			<Header />
