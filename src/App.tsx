ee0f28f39571cde8d348ad2eec6265de88fced75
LucArte <96082778+Lucarte@users.noreply.github.com>
2024-10-30 05:58:58 +0100

    debug log

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import Registration from "./pages/Registration";
 import "./App.css";
 import Header from "./partials/Header";
+import { useEffect } from "react";
 
 const App = () => {
-	console.log(
-		import.meta.env.VITE_MEILISEARCH_HOST,
-		import.meta.env.VITE_MEILISEARCH_API_KEY
-	);
+	useEffect(() => {
+		console.log(
+			"VITE_MEILISEARCH_HOST:",
+			import.meta.env.VITE_MEILISEARCH_HOST,
+			"VITE_MEILISEARCH_API_KEY:",
+			import.meta.env.VITE_MEILISEARCH_API_KEY
+		);
+	}, []);
+
 	return (
 		<div className=''>
 			<Header />
