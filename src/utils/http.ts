5f4131073509becdcb53172efa21479ad0b088ae
LucArte <96082778+Lucarte@users.noreply.github.com>
2024-11-16 21:18:51 +0100

    correct content in http helfer

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,9 +1,20 @@
 import axios from "axios";
 
+// Safely retrieve the CSRF token from meta tag
+const csrfToken =
+	(
+		document.head.querySelector(
+			'meta[name="csrf-token"]'
+		) as HTMLMetaElement | null
+	)?.content || "";
+
+// Create an Axios instance with the CSRF token in the headers
 const http = axios.create({
-	baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000", // Fallback for development or troubleshooting
+	baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
 	withCredentials: true,
-	withXSRFToken: true,
+	headers: {
+		"X-CSRF-TOKEN": csrfToken, // CSRF token, fallback to empty string if not found
+	},
 });
 
 export default http;
