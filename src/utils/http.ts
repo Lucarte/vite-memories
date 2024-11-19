b3d8b66da9ec7043de14af844a7fe1b7ac804ec6
LucArte <96082778+Lucarte@users.noreply.github.com>
2024-11-19 11:33:30 +0100

    back to axios for login

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -5,4 +5,18 @@ const http = axios.create({
 	withCredentials: true, // Ensures cookies are sent
 });
 
+// Interceptor to add CSRF token to headers for every request
+http.interceptors.request.use(
+	(config) => {
+		const csrfToken = document.cookie.split("XSRF-TOKEN=")[1]; // Extract CSRF token from cookies
+		if (csrfToken) {
+			config.headers["X-CSRF-TOKEN"] = csrfToken; // Attach CSRF token to request headers
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
+
 export default http;
