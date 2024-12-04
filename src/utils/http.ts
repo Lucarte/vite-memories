23bc5726d67fef59dedac6d8e829f18d7d9ceebf
LucArte <96082778+Lucarte@users.noreply.github.com>
2024-12-04 10:43:14 +0100

    simplify http fn

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -5,28 +5,4 @@ const http = axios.create({
 	withCredentials: true, // Ensures cookies are sent
 });
 
-// Interceptor to add CSRF token to headers for every request
-http.interceptors.request.use(
-	(config) => {
-		const csrfToken = document.cookie.split("XSRF-TOKEN=")[1]; // Extract CSRF token from cookies
-		if (csrfToken) {
-			config.headers["X-CSRF-TOKEN"] = csrfToken; // Attach CSRF token to request headers
-		}
-		return config;
-	},
-	(error) => {
-		return Promise.reject(error);
-	}
-);
-
-// Call this endpoint at the start of the session
-http
-	.get("/sanctum/csrf-cookie")
-	.then(() => {
-		console.log("CSRF cookie set successfully.");
-	})
-	.catch((err) => {
-		console.error("Failed to set CSRF cookie:", err);
-	});
-
 export default http;
